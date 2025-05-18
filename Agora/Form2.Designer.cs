535bb089b1db47b598918a39111ec95299a67aaa
ThatOneGriff <temka.pervakov@gmail.com>
2025-05-18 16:16:27 +0300

    even more planning yay

--- Agora/Form2.Designer.cs
+++ Agora/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Agora
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Agora";
-            this.ResumeLayout(false);
-
+            this.Text = "Form2";
         }
 
         #endregion
     }
 }
-
--- Agora/Registration.cs
+++ Agora/Registration.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Agora
 {
-    public partial class Form1 : Form
+    public partial class Registration : Form
     {
-        public Form1()
+        public Registration()
         {
             InitializeComponent();
         }
