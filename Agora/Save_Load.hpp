5fa9192972b40a35da4cb951ae0b49f42802cb78
ThatOneGriff <temka.pervakov@gmail.com>
2025-06-15 11:11:18 +0300

    SAVING DONEEEEEE

--- Agora/Save_Load.hpp
+++ Agora/Save_Load.hpp
@@ -2,6 +2,7 @@
 #ifndef SAVE_LOAD_HPP
 #define SAVE_LOAD_HPP
 
+#include <codecvt> // Locale things ('save' func)
 #include <fstream>
 #include <string>
 
@@ -20,11 +21,16 @@ void  save(User* user, std::wstring address = L"");
 }*/
 
 
+/// 'User*' instead of raw text as a protection from saving jackshit
 void save(User* user, std::wstring address/* = L""*/)
 {
 	if (address == L"")
 		address = user->name->as_filename(L".txt");
 	std::wofstream savefile(address);
+	// Copied from: https://stackoverflow.com/a/3950840/15540979
+	std::locale loc(std::locale::classic(), new std::codecvt_utf8<wchar_t>);
+	savefile.imbue(loc);
+	// The copied code deals with locales, enabling a full save into file without errors.
 	savefile << user->serialize();
 	savefile.close();
 }
