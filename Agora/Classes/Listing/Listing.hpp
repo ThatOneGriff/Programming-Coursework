2fec79068941c5739627b876eb839915875fb578
ThatOneGriff <temka.pervakov@gmail.com>
2025-06-16 05:07:54 +0300

    starting feed

---
+++ Agora/Classes/Listing/Listing.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef LISTING_HPP
+#define LISTING_HPP
+
+#include <string>
+
+#include "..\User.hpp"
+
+/// Represents an unsigned (1 user), signed (2 users), or completed contract.
+
+
+class Listing
+{
+public:
+
+	std::wstring name, description;
+	unsigned int payment, length_days;
+
+	///   заказчик   подрядчик
+	User *customer, *contractor;
+
+	Listing(const std::wstring& _name, const std::wstring& _description, const unsigned int _payment, const int _length_days)
+	: name(_name), description(_description), payment(_payment), length_days(_length_days)
+	{}
+};
+
+
+#endif
--- Agora/Main_Menu.hpp
+++ Agora/Main_Menu.hpp
@@ -66,6 +66,22 @@ private: System::Windows::Forms::TextBox^ output_account_extra;
 
 private: System::Windows::Forms::Button^ button1;
 private: System::Windows::Forms::TextBox^ output_account_website;
+private: System::Windows::Forms::Panel^ menu_feed;
+private: System::Windows::Forms::Label^ label_feed;
+private: System::Windows::Forms::PictureBox^ bg_feed;
+private: System::Windows::Forms::GroupBox^ group_active_duties;
+
+
+private: System::Windows::Forms::GroupBox^ group_offers;
+private: System::Windows::Forms::GroupBox^ group_orders;
+private: System::Windows::Forms::Button^ button_feed_update;
+private: System::Windows::Forms::Timer^ timer;
+
+
+
+
+
+
 
 
 
@@ -76,6 +92,7 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Main_Menu::typeid));
 			this->menu_button_search = (gcnew System::Windows::Forms::Button());
 			this->sidebar = (gcnew System::Windows::Forms::Panel());
@@ -84,7 +101,9 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->menu_button_account = (gcnew System::Windows::Forms::Button());
 			this->menu_button_feed = (gcnew System::Windows::Forms::Button());
 			this->menu_account = (gcnew System::Windows::Forms::Panel());
+			this->group_active_duties = (gcnew System::Windows::Forms::GroupBox());
 			this->group_account_data = (gcnew System::Windows::Forms::GroupBox());
+			this->output_account_website = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->output_account_extra = (gcnew System::Windows::Forms::TextBox());
 			this->output_account_email = (gcnew System::Windows::Forms::TextBox());
@@ -92,11 +111,19 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->output_account_name = (gcnew System::Windows::Forms::TextBox());
 			this->label_my_account = (gcnew System::Windows::Forms::Label());
 			this->bg_my_account = (gcnew System::Windows::Forms::PictureBox());
-			this->output_account_website = (gcnew System::Windows::Forms::TextBox());
+			this->menu_feed = (gcnew System::Windows::Forms::Panel());
+			this->button_feed_update = (gcnew System::Windows::Forms::Button());
+			this->group_orders = (gcnew System::Windows::Forms::GroupBox());
+			this->group_offers = (gcnew System::Windows::Forms::GroupBox());
+			this->label_feed = (gcnew System::Windows::Forms::Label());
+			this->bg_feed = (gcnew System::Windows::Forms::PictureBox());
+			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
 			this->sidebar->SuspendLayout();
 			this->menu_account->SuspendLayout();
 			this->group_account_data->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bg_my_account))->BeginInit();
+			this->menu_feed->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bg_feed))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// menu_button_search
@@ -108,7 +135,7 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->menu_button_search->Location = System::Drawing::Point(5, 9);
 			this->menu_button_search->Name = L"menu_button_search";
 			this->menu_button_search->Size = System::Drawing::Size(75, 75);
-			this->menu_button_search->TabIndex = 0;
+			this->menu_button_search->TabIndex = 1;
 			this->menu_button_search->UseVisualStyleBackColor = false;
 			// 
 			// sidebar
@@ -171,12 +198,14 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->menu_button_feed->Location = System::Drawing::Point(5, 93);
 			this->menu_button_feed->Name = L"menu_button_feed";
 			this->menu_button_feed->Size = System::Drawing::Size(75, 75);
-			this->menu_button_feed->TabIndex = 1;
+			this->menu_button_feed->TabIndex = 0;
 			this->menu_button_feed->UseVisualStyleBackColor = false;
+			this->menu_button_feed->Click += gcnew System::EventHandler(this, &Main_Menu::sidebar_pick_feed);
 			// 
 			// menu_account
 			// 
 			this->menu_account->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->menu_account->Controls->Add(this->group_active_duties);
 			this->menu_account->Controls->Add(this->group_account_data);
 			this->menu_account->Controls->Add(this->label_my_account);
 			this->menu_account->Controls->Add(this->bg_my_account);
@@ -184,6 +213,15 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->menu_account->Name = L"menu_account";
 			this->menu_account->Size = System::Drawing::Size(534, 415);
 			this->menu_account->TabIndex = 2;
+			// 
+			// group_active_duties
+			// 
+			this->group_active_duties->Location = System::Drawing::Point(281, 157);
+			this->group_active_duties->Name = L"group_active_duties";
+			this->group_active_duties->Size = System::Drawing::Size(248, 250);
+			this->group_active_duties->TabIndex = 5;
+			this->group_active_duties->TabStop = false;
+			this->group_active_duties->Text = L"Активные подряды";
 			// 
 			// group_account_data
 			// 
@@ -199,6 +237,16 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->group_account_data->TabIndex = 2;
 			this->group_account_data->TabStop = false;
 			this->group_account_data->Text = L"Данные";
+			// 
+			// output_account_website
+			// 
+			this->output_account_website->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
+			this->output_account_website->Location = System::Drawing::Point(6, 118);
+			this->output_account_website->Name = L"output_account_website";
+			this->output_account_website->ReadOnly = true;
+			this->output_account_website->Size = System::Drawing::Size(255, 26);
+			this->output_account_website->TabIndex = 4;
 			// 
 			// button1
 			// 
@@ -277,15 +325,76 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->bg_my_account->TabIndex = 1;
 			this->bg_my_account->TabStop = false;
 			// 
-			// output_account_website
+			// menu_feed
 			// 
-			this->output_account_website->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular,
-				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			this->output_account_website->Location = System::Drawing::Point(6, 118);
-			this->output_account_website->Name = L"output_account_website";
-			this->output_account_website->ReadOnly = true;
-			this->output_account_website->Size = System::Drawing::Size(255, 26);
-			this->output_account_website->TabIndex = 4;
+			this->menu_feed->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->menu_feed->Controls->Add(this->button_feed_update);
+			this->menu_feed->Controls->Add(this->group_orders);
+			this->menu_feed->Controls->Add(this->group_offers);
+			this->menu_feed->Controls->Add(this->label_feed);
+			this->menu_feed->Controls->Add(this->bg_feed);
+			this->menu_feed->Location = System::Drawing::Point(12, 12);
+			this->menu_feed->Name = L"menu_feed";
+			this->menu_feed->Size = System::Drawing::Size(534, 415);
+			this->menu_feed->TabIndex = 3;
+			// 
+			// button_feed_update
+			// 
+			this->button_feed_update->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button_feed_update.BackgroundImage")));
+			this->button_feed_update->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->button_feed_update->Location = System::Drawing::Point(265, 6);
+			this->button_feed_update->Name = L"button_feed_update";
+			this->button_feed_update->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->button_feed_update->Size = System::Drawing::Size(35, 35);
+			this->button_feed_update->TabIndex = 0;
+			this->button_feed_update->UseVisualStyleBackColor = true;
+			this->button_feed_update->Click += gcnew System::EventHandler(this, &Main_Menu::update_feed);
+			// 
+			// group_orders
+			// 
+			this->group_orders->Location = System::Drawing::Point(274, 157);
+			this->group_orders->Name = L"group_orders";
+			this->group_orders->Size = System::Drawing::Size(255, 253);
+			this->group_orders->TabIndex = 6;
+			this->group_orders->TabStop = false;
+			this->group_orders->Text = L"Ищут подрядчика";
+			// 
+			// group_offers
+			// 
+			this->group_offers->Location = System::Drawing::Point(8, 157);
+			this->group_offers->Name = L"group_offers";
+			this->group_offers->Size = System::Drawing::Size(255, 253);
+			this->group_offers->TabIndex = 5;
+			this->group_offers->TabStop = false;
+			this->group_offers->Text = L"Ищут заказчика";
+			// 
+			// label_feed
+			// 
+			this->label_feed->AutoSize = true;
+			this->label_feed->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(232)), static_cast<System::Int32>(static_cast<System::Byte>(232)),
+				static_cast<System::Int32>(static_cast<System::Byte>(232)));
+			this->label_feed->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label_feed->Location = System::Drawing::Point(3, 8);
+			this->label_feed->Name = L"label_feed";
+			this->label_feed->Size = System::Drawing::Size(247, 29);
+			this->label_feed->TabIndex = 0;
+			this->label_feed->Text = L"Лента объявлений";
+			// 
+			// bg_feed
+			// 
+			this->bg_feed->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bg_feed.BackgroundImage")));
+			this->bg_feed->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->bg_feed->Location = System::Drawing::Point(-1, -1);
+			this->bg_feed->Name = L"bg_feed";
+			this->bg_feed->Size = System::Drawing::Size(534, 152);
+			this->bg_feed->TabIndex = 1;
+			this->bg_feed->TabStop = false;
+			// 
+			// timer
+			// 
+			this->timer->Interval = 500;
+			this->timer->Tick += gcnew System::EventHandler(this, &Main_Menu::update_feed);
 			// 
 			// Main_Menu
 			// 
@@ -293,8 +402,9 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Control;
 			this->ClientSize = System::Drawing::Size(639, 437);
-			this->Controls->Add(this->menu_account);
 			this->Controls->Add(this->sidebar);
+			this->Controls->Add(this->menu_feed);
+			this->Controls->Add(this->menu_account);
 			this->DoubleBuffered = true;
 			this->Name = L"Main_Menu";
 			this->Text = L"Agora";
@@ -304,6 +414,9 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 			this->group_account_data->ResumeLayout(false);
 			this->group_account_data->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bg_my_account))->EndInit();
+			this->menu_feed->ResumeLayout(false);
+			this->menu_feed->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bg_feed))->EndInit();
 			this->ResumeLayout(false);
 
 		}
@@ -311,6 +424,28 @@ private: System::Windows::Forms::TextBox^ output_account_website;
 		#pragma endregion
 
 private:
+
+	#pragma region ======= Feed =======
+
+	void sidebar_pick_feed(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->Text = "Agora: Лента";
+		menu_feed->BringToFront();
+
+		//fill_feed_menu();
+	}
+
+
+	void update_feed(System::Object^ _sender, System::EventArgs^ e)
+	{
+		group_orders->Enabled = ! group_orders->Enabled;
+		group_offers->Enabled = ! group_offers->Enabled;
+		timer->Enabled = ! timer->Enabled;
+	}
+
+	#pragma endregion
+
+
 
 	#pragma region ======= Account =======
 
